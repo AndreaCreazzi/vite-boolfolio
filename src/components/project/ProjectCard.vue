4145c3df4c80a03d7d7b068ff40b783454871ef8
Andrea Creazzi <andreacreazzi@libero.it>
2023-09-11 16:09:44 +0200

    add projectDate feature

--- src/components/project/ProjectCard.vue
+++ src/components/project/ProjectCard.vue
@@ -1,6 +1,21 @@
 <script>
 export default {
     props: { project: Object },
+    computed: {
+        projectDate() {
+            const date = new Date(this.project.created_at);
+            let day = date.getDate();
+            let month = date.getMonth();
+            const year = date.getFullYear();
+            const hours = date.getHours();
+            const minute = date.getMinutes();
+            const seconds = date.getSeconds();
+            day = day < 10 ? '0' + day : day;
+            month = month < 10 ? '0' + month : month;
+            const projectDate = `${day}/${month}/${year} alle ${hours}:${minute}:${seconds}`;
+            return projectDate;
+        }
+    }
 }
 </script>
 <template>
@@ -11,8 +26,9 @@ export default {
         <div class="card-body">
             <p>{{ project.description }}</p>
         </div>
-        <div class="card-footer">
+        <div class="card-footer d-flex justify-content-between align-items-center">
             <a :href="project.link">{{ project.link }}</a>
+            <p>Pubblicato il: <time>{{ projectDate }}</time></p>
         </div>
     </div>
 </template>
