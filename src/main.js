ac9cfb95bdad3003dab2185c701a5654669cd8ed
Andrea Creazzi <andreacreazzi@libero.it>
2023-09-13 17:41:21 +0200

    add validator features

--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@ import { createApp } from "vue";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { router } from "./router";
 import App from "./App.vue";
+import AppAlert from "./components/project/AppAlert.vue";
 
-createApp(App).use(router).mount("#app");
+const app = createApp(App);
+app.component("AppAlert", AppAlert);
+app.use(router);
+app.mount("#app");
--- src/pages/ContactPages.vue
+++ src/pages/ContactPages.vue
@@ -3,21 +3,57 @@ import axios from 'axios';
 const endpoint = 'http://127.0.0.1:8000/api/contact-message';
 const emptyForm = { email: '', subject: '', message: '' };
 export default {
-    data: () => ({ form: emptyForm }),
+    data: () => ({
+        form: emptyForm,
+        errors: {},
+        successMessage: null
+    }),
+    computed: {
+        hasErrors() {
+            return Object.keys(this.errors).length;
+        },
+        showAlert() {
+            return Boolean(this.hasErrors || this.successMessage);
+        },
+        alertType() {
+            return this.hasErrors ? 'danger' : 'success';
+        }
+    },
     methods: {
         sendForm() {
+            this.errors = {};
+            this.successMessage = null;
             axios.post(endpoint, this.form)
-                .then(() => { this.form = emptyForm })
-                .catch(err => { console.error(err) })
+                .then(() => {
+                    this.form = emptyForm;
+                    this.successMessage = 'Messaggio inviato con successo'
+                })
+                .catch(err => {
+                    if (err.response.status === 400) {
+                        const { errors } = err.response.data;
+                        const errorMessage = {};
+                        for (let field in errors) errorMessage[field] = errors[field][0];
+                        this.errors = errorMessage;
+                    } else {
+                        console.error(err);
+                        this.errors = { network: 'Si è verificato un errore!' }
+                    }
+                });
         }
-    }
+    },
 };
 </script>
 
 <template>
     <div class="container">
         <h2 class="my-5">Contattaci</h2>
-        <form @submit.prevent="sendForm">
+        <AppAlert :isOpen="showAlert" :type="alertType">
+            <div v-if="successMessage">{{ successMessage }}</div>
+            <ul v-if="hasErrors">
+                <li v-for="(error, field) in errors" :key="field">{{ error }}</li>
+            </ul>
+        </AppAlert>
+        <form @submit.prevent="sendForm" novalidate>
             <div class="form-floating mb-3">
                 <input type="email" class="form-control" id="email" placeholder="name@example.com"
                     v-model.trim="form.email">
